c6668d2e3de9fb5430ec8b2013900185cbe81161
zhuonima <tz.zhuo@protonmail.com>
2017-11-03 14:30:38 +0800

    配置hello-service

--- eureka-server/start-all-eureka-server.sh
+++ eureka-server/start-all-eureka-server.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod1 &
+
+nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod2 &
+
+nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod3 &
