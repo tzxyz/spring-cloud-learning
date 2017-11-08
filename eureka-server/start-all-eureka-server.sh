10351e0151d119fcf953e20597b11bacbc5738bc
zhuonima <tz.zhuo@protonmail.com>
2017-11-08 18:01:53 +0800

    添加API网关服务

--- eureka-server/start-all-eureka-server.sh
+++ eureka-server/start-all-eureka-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod1 &
+nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod1 > eureka-server-1.log &
 
-nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod2 &
+nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod2 > eureka-server-2.log &
 
-nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod3 &
+nohup mvn spring-boot:run -Dspring.cloud.config.profile=prod3 > eureka-server-3.log &
